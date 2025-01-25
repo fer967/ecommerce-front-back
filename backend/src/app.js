488759289169cc4c8486403fe2b902d4f93c0165
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-01-25 19:08:47 -0300

    agrego cors y variables de entorno

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,18 @@ const productosRouter = require("./routes/productos.router.js");
 const ordenesRouter = require("./routes/ordenes.router.js");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const connect = mongoose.connect("mongodb+srv://gabriel70080:coder@cluster0.yt1in.mongodb.net/react?retryWrites=true&w=majority&appName=Cluster0")
+const connect = mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Conectado a la DB") );
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api/productos", productosRouter);
 app.use("/api/ordenes", ordenesRouter);
