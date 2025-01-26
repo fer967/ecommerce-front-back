3a51c392b629ae8da095354d5b837ce841dd304b
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-01-26 12:27:56 -0300

    modifico axios

--- ecommerce-react/src/services/config.js
+++ ecommerce-react/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Cambiar la URL por la del servidor backend
+  baseURL: 'http://localhost:3000/api', // Cambiar la URL por la del servidor backend
   timeout: 5000 
 });
 
