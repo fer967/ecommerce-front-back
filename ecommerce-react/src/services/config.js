16a78fff5044cb516c94b5b057936ebf0ab50f1e
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-02-01 12:01:56 -0300

    cambios para vistas responsive

--- ecommerce-react/src/services/config.js
+++ ecommerce-react/src/services/config.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://ecommerce-front-back-6.onrender.com/api", // Cambiar la URL por la del servidor backend
-  //baseURL: "http://localhost:3000/api", // Cambiar la URL por la del servidor backend
+  baseURL: "https://ecommerce-front-back-6.onrender.com/api", // Cambiar la URL por la del servidor backend (render)
+  //baseURL: "http://localhost:3000/api", // Cambiar la URL por la del servidor backend (local)
   timeout: 5000 
 });
 
