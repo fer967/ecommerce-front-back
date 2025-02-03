1146fcbebad4959e3cdcc4550434d78d0053b241
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2025-02-03 16:31:41 -0300

    estilos al carrito

--- ecommerce-react/src/componentes/CartWidget/CartWidget.jsx
+++ ecommerce-react/src/componentes/CartWidget/CartWidget.jsx
@@ -13,35 +13,5 @@ const CartWidget = () => {
 }
 
 export default CartWidget;
-/*
 
-*/
 
-/*
-import { useContext } from 'react';
-import'./CartWidget.css';
-import { CartContext } from '../../context/CartContext';
-import { Link } from 'react-router-dom';
-
-export default function CartWidget(){
-    const{totalCont} = useContext(CartContext);
-    return(
-        <Link to='/cart' style={{display: totalCont > 0? 'block':'none'}} >
-            <button className='icono'>🛒{totalCont}</button>
-        </Link>
-    );
-}
-*/
-
-/*
-return (
-        <div>
-            <Link to="/cart">
-                <img className="carrito" src="../img/carro.png" alt="Carrito" />
-                {
-                    cantidadTotal > 0 && <strong> {cantidadTotal} </strong>
-                }
-            </Link>
-        </div>
-    )
-*/
--- ecommerce-react/src/context/CarritoContext.jsx
+++ ecommerce-react/src/context/CarritoContext.jsx
@@ -54,49 +54,9 @@ export const CarritoProvider = ({ children }) => {
 }
 
 
-/*
-import { createContext, useState,useEffect } from 'react';
-export const CartContext = createContext({cart:[]});
 
-export function CartProvider({ children }) {
-    const [cart, setCart] = useState([]);
-    console.log(cart)
-    
-    const addItem = (item, cont)=>{
-        if(!isInCart(item.id)){
 
-            // modificacion
-            setCart([...cart,{...item,cont}]);
 
-        } else{
-            console.error('el producto ya fue agregado');
-        }
-    }
-
-    const removeItem = (itemId)=>{
-        const cartUpdated = cart.filter(prod => prod.id !== itemId)
-        setCart(cartUpdated)
-    }
-
-    const clearCart = ()=>{
-        setCart([])
-    }
-
-    const isInCart = (itemId)=>{
-        return cart.some(prod => prod.id === itemId)
-    }
-
-    const total =  cart.reduce((acc,el) => acc + el.price * el.cont, 0);
-
-    const totalCont = cart.reduce((acc,el) => acc + el.cont, 0);
-
-    return (
-        <CartContext.Provider value={{cart,addItem,removeItem,clearCart,total,totalCont}}>
-            {children}
-        </CartContext.Provider>
-    );
-}
-*/
 
 
 
